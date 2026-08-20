e018986998ffa88f0f546fc91c4e754b3a7c65b1
pragun bhandari <pragun.ba99@outlook.com>
2026-08-20 12:17:48 +0100

    format backend with ruff

--- backend/tests/test_health.py
+++ backend/tests/test_health.py
@@ -4,7 +4,8 @@ from backend.app.main import app
 
 client = TestClient(app)
 
+
 def test_health():
     response = client.get("/health")
     assert response.status_code == 200
-    assert response.json() == {"status":"ok"}
+    assert response.json() == {"status": "ok"}
